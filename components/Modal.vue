65e407a575059cf54041950d98762398bdd686e0
Daniil <62702483+NocokCydbi@users.noreply.github.com>
2023-04-17 20:31:31 +0300

    test: modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -52,7 +52,7 @@ export default {
         },
     },
     created(){
-        console.log(`created: ${this.$route}`);
+        console.log(`created: ${this.$route.query}`);
     if (this.$route.query.city)  {
         this.isOpen = false
       }
