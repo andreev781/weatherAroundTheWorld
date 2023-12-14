91443cd65ca2697266f82602f1cafebc0515e4e4
andreev781 <daniil03022006dan@gmail.com>
2023-12-14 06:08:15 +0300

    changed deploy

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,138 +1,154 @@
 <template>
-    <div class="modal" v-if="isOpen">
-        <div class="modal__wrapper">
-            <div class="modal__container">
-                <div class="modal__top">
-                    <h4 class="modal__title">
-                        Your city is {{city}}?
-                    </h4>
-                    <span class="modal__close" @click="isOpen = false" >&#10006;</span>
-                </div>
-
-            <div class="modal__input" v-if="showInput">
-                <input @focus="error = ''"  @keypress="isLetter($event)" @keyup.enter="changeCity" type="text" placeholder="Type your city.." v-model="otherCity">
-                <span  class="error modal__error">{{error ? error : ''}}</span>
-
-                <button class="modal__button modal__button_green"  @click="changeCity">
-                    Ok
-                </button>
-            </div>
-            <div class="modal__buttons" v-else>
-                <button class="modal__button modal__button_green" @click="isOpen = false">Yes</button>
-                <button class="modal__button modal__button_red" @click="showInput = true">No</button>
-            </div>
-            </div>
+  <div class="modal" v-if="isOpen">
+    <div class="modal__wrapper">
+      <div class="modal__container">
+        <div class="modal__top">
+          <h4 class="modal__title">Your city is {{ city }}?</h4>
+          <span class="modal__close" @click="isOpen = false">&#10006;</span>
         </div>
+
+        <div class="modal__input" v-if="showInput">
+          <input
+            @focus="error = ''"
+            @keypress="isLetter($event)"
+            @keyup.enter="changeCity"
+            type="text"
+            placeholder="Type your city.."
+            v-model="otherCity"
+          />
+          <span class="error modal__error">{{ error ? error : "" }}</span>
+
+          <button class="modal__button modal__button_green" @click="changeCity">
+            Ok
+          </button>
+        </div>
+        <div class="modal__buttons" v-else>
+          <button
+            class="modal__button modal__button_green"
+            @click="isOpen = false"
+          >
+            Yes
+          </button>
+          <button
+            class="modal__button modal__button_red"
+            @click="showInput = true"
+          >
+            No
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default {        
-    name: 'Modal',
-    props: ['city'],
-    data() {
-        return {
-            isOpen: false,
-            showInput: false,
-            otherCity: '',
-            error: ''
-        }
+export default {
+  name: "Modal",
+  props: ["city"],
+  data() {
+    return {
+      isOpen: false,
+      showInput: false,
+      otherCity: "",
+      error: "",
+    };
+  },
+  methods: {
+    isLetter(e) {
+      let char = String.fromCharCode(e.keyCode);
+      if (/^[A-Za-z\s+]$/.test(char)) return true;
+      else e.preventDefault();
     },
-    methods: {
-        isLetter(e){
-            let char = String.fromCharCode(e.keyCode);
-            if (/^[A-Za-z\s+]$/.test(char)) return true;
-            else e.preventDefault();
-        },
-        changeCity(){
-            if (!this.otherCity) this.error = ('Type your city');
-            else {
-                this.$store.dispatch('client/checkCity', this.otherCity)
-                .then(res => res ? '' : this.error = this.$store.getters['client/getError'])
-            }
-        },
-    },
-    created(){
-        console.log(`created: ${this.$route.query.city}`);
-    if (this.$route.query.city)  {
-        this.isOpen = false
+    changeCity() {
+      if (!this.otherCity) this.error = "Type your city";
+      else {
+        this.$store
+          .dispatch("client/checkCity", this.otherCity)
+          .then((res) =>
+            res ? "" : (this.error = this.$store.getters["client/getError"])
+          );
       }
-      else this.isOpen = true  
     },
-    watch: {
-        isOpen: function() {
-            if (this.isOpen) {
-                document.documentElement.style.overflow = 'hidden'
-                return
-            }
-            document.documentElement.style.overflow = 'auto'
-        }
-    }
-}
+  },
+  created() {
+    if (this.$route.query.city) {
+      this.isOpen = false;
+    } else this.isOpen = true;
+  },
+  watch: {
+    isOpen: function () {
+      if (this.isOpen) {
+        document.documentElement.style.overflow = "hidden";
+        return;
+      }
+      document.documentElement.style.overflow = "auto";
+    },
+  },
+};
 </script>
 <style>
-    .modal {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: table;
-    transition: opacity 0.3s ease;
-    }
-    .modal__wrapper {
-    display: table-cell;
-    vertical-align: middle;
-    }
-    .modal__container {
-        width: 250px;
-        margin: 0 auto;
-        padding: 20px 40px;
-        background: #fff;
-        color: #000;
-        border-radius: 5px;
-    }
-    .modal__title {
-        margin: 0;
-        text-align: center;
-        font-size: 20px;
-    }
-    .modal__top {
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 20px;
-    }
-    .modal__close {
-        display: block;
-        cursor: pointer;
-        padding: 0;
-        margin: 0;
-    }
-    .modal__button {
-        font-size: 16px;
-        background: transparent;
-        box-shadow: none;
-        cursor: pointer;
-        padding: 5px 20px;
-        color: #fff;
-        border: none;
-    }
-    .modal__button_green {
-        margin-right: 10px;
-        background: #47A76A;
-    }
-    .modal__button_red {
-        background: #E32636;
-    }
-    .modal__input input {
-        display: block;
-        padding: 5px;
-        outline: none;
-        margin-bottom: 10px;
-    }
-    .modal__error {
-        display: block;
-        margin-bottom: 5px !important;
-    }
+.modal {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+.modal__wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+.modal__container {
+  width: 250px;
+  margin: 0 auto;
+  padding: 20px 40px;
+  background: #fff;
+  color: #000;
+  border-radius: 5px;
+}
+.modal__title {
+  margin: 0;
+  text-align: center;
+  font-size: 20px;
+}
+.modal__top {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+.modal__close {
+  display: block;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+}
+.modal__button {
+  font-size: 16px;
+  background: transparent;
+  box-shadow: none;
+  cursor: pointer;
+  padding: 5px 20px;
+  color: #fff;
+  border: none;
+}
+.modal__button_green {
+  margin-right: 10px;
+  background: #47a76a;
+}
+.modal__button_red {
+  background: #e32636;
+}
+.modal__input input {
+  display: block;
+  padding: 5px;
+  outline: none;
+  margin-bottom: 10px;
+}
+.modal__error {
+  display: block;
+  margin-bottom: 5px !important;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,67 @@
 <template>
   <div class="container">
-    <Main :location="weatherData.location" :currentWeather="weatherData.current" />
-    <Forecast :forecast="weatherData.forecast.forecastday"/>
+    <Main
+      :location="weatherData.location"
+      :currentWeather="weatherData.current"
+    />
+    <Forecast :forecast="weatherData.forecast.forecastday" />
+    <Modal :city="weatherData.location.region" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
-  async asyncData({route, $axios}) {
-    let ip = await $axios.get('https://api.ipify.org/?format=json')
-    let location = await $axios.get(`https://api.weatherapi.com/v1/ip.json?key=cf424b291f6e48d5ac9182353220103&q=${ip.data.ip}`);
-    let city = location.data.city
-    let weatherData = false
-    if(route.query.city) city = route.query.city
-    console.log(route);
-    await $axios.get(`https://api.weatherapi.com/v1/forecast.json?key=cf424b291f6e48d5ac9182353220103&q=${city}&days=3`)
-      .then(res => weatherData = res.data)
-      .catch(async function() {
-        route.query.city = ''
-        city = location.data.city
-        await $axios.get(`https://api.weatherapi.com/v1/forecast.json?key=cf424b291f6e48d5ac9182353220103&q=${city}&days=3`)
-          .then(res => weatherData = res.data)
-    })
-
-    return {weatherData}
-  }
-}
+  name: "IndexPage",
+  async asyncData({ route, $axios }) {
+    let city = "Moscow";
+    let weatherData = false;
+    if (route.query.city) city = route.query.city;
+    await $axios
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=cf424b291f6e48d5ac9182353220103&q=${city}&days=3`
+      )
+      .then((res) => (weatherData = res.data))
+      .catch(async function () {
+        route.query.city = "";
+        await $axios
+          .get(
+            `https://api.weatherapi.com/v1/forecast.json?key=cf424b291f6e48d5ac9182353220103&q=${city}&days=3`
+          )
+          .then((res) => (weatherData = res.data));
+      });
+      
+    return { weatherData };
+  },
+};
 </script>
 <style>
-  h1 {
-    margin-top: 0;
-  }
-  body {
-    position: relative;
-  background: linear-gradient(to bottom, rgb(43,50,178) 0%, rgb(20,136,204) 100%);
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    margin: 0;
-    
-  }
-  .container {
-    max-width: 880px;
-    overflow: hidden;
-    margin: 8px auto;
-    padding: 0 10px;
-    margin-top: 0;
-    font-size: 1.1em;
-    color: #fff;
-  }
-  .error {
-    margin: 0;
-    font-size: 12px;
-    color: red
-  }
+h1 {
+  margin-top: 0;
+}
+body {
+  position: relative;
+  background: linear-gradient(
+    to bottom,
+    rgb(43, 50, 178) 0%,
+    rgb(20, 136, 204) 100%
+  );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  margin: 0;
+}
+.container {
+  max-width: 880px;
+  overflow: hidden;
+  margin: 8px auto;
+  padding: 0 10px;
+  margin-top: 0;
+  font-size: 1.1em;
+  color: #fff;
+}
+.error {
+  margin: 0;
+  font-size: 12px;
+  color: red;
+}
 </style>
 
